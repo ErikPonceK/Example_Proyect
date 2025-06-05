d6272fcbd243f4fb6c61922fe87188ebe45cf2e8
Ponce <erik.poncemeza@hotmail.com>
2025-06-05 15:32:26 -0600

    fix configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     kotlin("jvm")
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 dependencies {
